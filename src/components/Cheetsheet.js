82e32b05af832ac94caace7bc66963adb27bc82f
Pjha72 <pjha76737@gmail.com>
2024-05-31 12:55:29 +0530

    Changes in cheatsheet

--- src/components/Cheetsheet.js
+++ src/components/Cheetsheet.js
@@ -23,24 +23,18 @@ export default function Cheetsheet() {
               </div>
             </div>
 
-            <div class="ml-6 items-left mb-3">
-              <div class="relative group">
-                <button class="flex items-center justify-between w-full rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+            <div class="w-full mt-4 flex bg-white dark:bg-dark_50 flex-col shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] false rounded-md border border-gray-200 dark:border-zinc-800">
+              <div class="px-3 py-2 border">
+                <button class="flex items-center justify-between w-full group mb-1" aria-expanded="false">
                   <div className="text-md  font-semibold text-brand">
                     Key Highlight
                   </div>
                   <div className="flex items-center gap-x-2">
                     {/* <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center"> */}
                       <svg
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        class="w-4 h-4"
-                        viewBox="0 0 24 24"
+                        className="w-8 h-8 flex-shrink-0 ml-3 rounded-lg text-sm border fill-current text-gray-400 group-hover:text-gray-500 dark:bg-dark_40 border-zinc-300 dark:border-zinc-800 bg-[#fafafa]"
                       >
-                        <path d="M6 9l6 6 6-6"></path>
+                        <path d="M16 20l-5.4-5.4 1.4-1.4 4 4 4-4 1.4 1.4z"></path>
                       </svg>
                     {/* </span> */}
                   </div>
@@ -60,27 +54,35 @@ export default function Cheetsheet() {
                 </div>
               </div>
             </div>
-            <div class="ml-6 items-left">
-              <div class="relative">
-                <select class=" block w-full rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                  <option>SM</option>
-                  <option>M</option>
-                  <option>L</option>
-                  <option>XL</option>
-                </select>
-                <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-4 h-4"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M6 9l6 6 6-6"></path>
-                  </svg>
-                </span>
+            <div class="w-full mt-4 flex bg-white dark:bg-dark_50 flex-col shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] false rounded-md border border-gray-200 dark:border-zinc-800">
+              <div class="px-3 py-2 border">
+                <button class="flex items-center justify-between w-full group mb-1" aria-expanded="false">
+                  <div className="text-md  font-semibold text-brand">
+                    Key Highlight
+                  </div>
+                  <div className="flex items-center gap-x-2">
+                    {/* <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center"> */}
+                      <svg
+                        className="w-8 h-8 flex-shrink-0 ml-3 rounded-lg text-sm border fill-current text-gray-400 group-hover:text-gray-500 dark:bg-dark_40 border-zinc-300 dark:border-zinc-800 bg-[#fafafa]"
+                      >
+                        <path d="M16 20l-5.4-5.4 1.4-1.4 4 4 4-4 1.4 1.4z"></path>
+                      </svg>
+                    {/* </span> */}
+                  </div>
+                </button>
+                <div className="text-sm text-gray-900 false">
+                    <div className="flex flex-col text-lg space-y-4 p-4">
+                        <div className="flex flex-col items-start gap-y-1">
+                            <li>Teaches you problem solving by diving deep into DSA, with 450+ modules.</li>
+                            <li>In-depth video solutions covering brute, better, optimal solutions.</li>
+                            <li>Well structured articles/notes for quick revision.</li>
+                            <li>C++, Java, Python, and Javascript code.</li>
+                            <li>Company tags associated with each problem.</li>
+                            <li>Notes section to save your notes for quick revision.</li>
+                            <li>Notes section to save your notes for quick revision.</li>
+                        </div>
+                    </div>
+                </div>
               </div>
             </div>
           </section>
