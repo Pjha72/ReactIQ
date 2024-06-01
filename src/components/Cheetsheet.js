cc623cc5f1845d64b52f4181133d2325f7f88ca1
Pjha72 <pjha76737@gmail.com>
2024-06-01 18:39:50 +0530

    Add Day 4-react hooks

--- src/components/Cheetsheet.js
+++ src/components/Cheetsheet.js
@@ -7,7 +7,7 @@ import {
 } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
-import {CodeBlock, dracula} from "react-code-blocks"
+import { CodeBlock, dracula } from "react-code-blocks";
 
 export default function Cheetsheet() {
   const [textColor, setTextColor] = useState("#000000");
@@ -305,11 +305,11 @@ export default function Cheetsheet() {
                     style={{ color: textColor3 }}
                   />
                 </DisclosureButton>
-                
+
                 <DisclosurePanel className="text-lg text-gray-700 false border-amber-700 text-justify">
-                <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      1. JavaScript XML (JSX)
-                </h2>
+                  <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
+                    1. JavaScript XML (JSX)
+                  </h2>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`// JSX Elements
@@ -340,8 +340,8 @@ return (
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      2. Functional Components
-                </h2>
+                    2. Functional Components
+                  </h2>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`import React from "react";
@@ -375,8 +375,8 @@ export default function Hello(props) {
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      3. Embed Components
-                </h2>
+                    3. Embed Components
+                  </h2>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`// Embed internal components
@@ -408,8 +408,8 @@ export default function UserProfile() {
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      4. CSS In a React
-                </h2>
+                    4. CSS In a React
+                  </h2>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`import React from "react"; 
@@ -426,10 +426,9 @@ export default function Student() {
                     />
                   </div>
 
-
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      5. React State
-                </h2>
+                    5. React State
+                  </h2>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`import React, { useState } from "react";
@@ -453,8 +452,8 @@ export default function Hello(props) {
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      6. React Forms
-                </h2>
+                    6. React Forms
+                  </h2>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`import React, { useState } from "react";
@@ -487,8 +486,8 @@ export default function LoginForm() {
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      7. Map Elements
-                </h2>
+                    7. Map Elements
+                  </h2>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`let elements = [{
@@ -533,10 +532,13 @@ return (
                   />
                 </DisclosureButton>
                 <DisclosurePanel className="text-lg text-gray-700 false border-amber-700 text-justify">
-                <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      1. Components and Hooks
-                </h2>
-                <p className="leading-relaxed">Use functional components and hooks (like useState and useEffect) for a more concise and modular code structure.</p>
+                  <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
+                    1. Components and Hooks
+                  </h2>
+                  <p className="leading-relaxed">
+                    Use functional components and hooks (like useState and
+                    useEffect) for a more concise and modular code structure.
+                  </p>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`import React, { useState, useEffect } from 'react';
@@ -555,9 +557,12 @@ const MyComponent = () → {
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      2. Conditional Rendering
-                </h2>
-                <p className="leading-relaxed">Render different components or elements based on conditions using the ternary operator.</p>
+                    2. Conditional Rendering
+                  </h2>
+                  <p className="leading-relaxed">
+                    Render different components or elements based on conditions
+                    using the ternary operator.
+                  </p>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`const MyComponent = ({ isloggedIn )) => { 
@@ -576,9 +581,12 @@ const MyComponent = () → {
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      3. Controlled Components
-                </h2>
-                <p className="leading-relaxed">Use controlled components to handle user input and maintain the input's state within the component.</p>
+                    3. Controlled Components
+                  </h2>
+                  <p className="leading-relaxed">
+                    Use controlled components to handle user input and maintain
+                    the input's state within the component.
+                  </p>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`import React, {useState} from 'react';
@@ -599,9 +607,12 @@ const InputForm = () => {
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      4. Mapping Data
-                </h2>
-                <p className="leading-relaxed">Map an array of data to JSX elements to dynamically render a list of items.</p>
+                    4. Mapping Data
+                  </h2>
+                  <p className="leading-relaxed">
+                    Map an array of data to JSX elements to dynamically render a
+                    list of items.
+                  </p>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`const MyList = ({ items }) ⇒ {
@@ -622,9 +633,12 @@ return (
                   </div>
 
                   <h2 class="text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
-                      5. Using Fragments
-                </h2>
-                <p className="leading-relaxed">Use React Fragments to avoid adding unnecessary wrapping elements when rendering multiple elements.</p>
+                    5. Using Fragments
+                  </h2>
+                  <p className="leading-relaxed">
+                    Use React Fragments to avoid adding unnecessary wrapping
+                    elements when rendering multiple elements.
+                  </p>
                   <div className="font-mono text-sm">
                     <CodeBlock
                       text={`const MyComponent = () ⇒ {
@@ -654,10 +668,10 @@ return (
                   onClick={handleTextColor5}
                 >
                   <span
-                    className="text-md font-semibold text-brand"
+                    className="text-lg font-semibold text-brand"
                     style={{ color: textColor5 }}
                   >
-                    Day4: Basic
+                    Day4: React Hooks
                   </span>
                   <ChevronDownIcon
                     className="size-5 fill-gray/60 group-data-[hover]:fill-gray/50 group-data-[open]:rotate-180 border-2 rounded border-emerald-400"
@@ -665,7 +679,225 @@ return (
                   />
                 </DisclosureButton>
                 <DisclosurePanel className="text-lg text-gray-700 false border-amber-700 text-justify">
-                  <section class="text-gray-600 body-font">
+                  <p className="leading-relaxed">
+                    React Hooks are built-in functions that allow you to use
+                    state and other React features, like lifecycle methods and
+                    context, in functional components without needing to write a
+                    class component.
+                  </p>
+                  <h2 class=" mt-4 text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
+                    1. useState Hooks
+                  </h2>
+                  <p className="leading-relaxed text-justify">
+                    This Hook allows you to add state to functional components.
+                    The <span className="text-[#ef443a]">useState</span>{" "}
+                    function returns a pair: the current state and a function
+                    that updates it.
+                  </p>
+                  <p className="text-pink-400 font-md"> Important Point : </p>
+                  <div className="text-justify">
+                    <li>
+                      State Name: <span className="text-rose-500">count</span>
+                    </li>
+                    <li>
+                      Function to set state:{" "}
+                      <span className="text-green-500">setCount</span>
+                    </li>
+                    <li>
+                      Initial value: <span className="text-blue-500">0</span>
+                    </li>
+                    <li>
+                      Updating state:{" "}
+                      <span className="text-yellow-500">
+                        setCount(count + 1)
+                      </span>
+                    </li>
+                  </div>
+
+                  <div className="font-mono text-sm ">
+                    <CodeBlock
+                      text={`import React, { useState } from 'react';
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}`}
+                      showLineNumbers
+                      codeBlock
+                      language="javascript"
+                      theme={dracula}
+                    />
+                  </div>
+
+                  <h2 class=" mt-4 text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
+                    2. useEffect Hooks
+                  </h2>
+                  <p className="leading-relaxed text-justify">
+                    This Hook lets you perform side effects in functional
+                    components. It's a close replacement for{" "}
+                    <span className="text-[#ef443a]">componentDidMount</span>,
+                    <span className="text-[#ef443a]"> componentDidUpdate</span>,
+                    and{" "}
+                    <span className="text-[#ef443a]">componentWillUnmount</span>{" "}
+                    in class components.
+                  </p>
+                  <p className="text-pink-400 font-md"> Important Point : </p>
+                  <div className="text-justify">
+                    <li>
+                      Dependency:{" "}
+                      <span className="text-rose-500">[count] </span> - This
+                      code will be executed when the dependency count is
+                      updated.
+                    </li>
+                  </div>
+
+                  <div className="font-mono text-sm ">
+                    <CodeBlock
+                      text={`import React, { useState, useEffect } from 'react';
+function Counter() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = You clicked {count} times;
+    return () => {
+      document.title = 'React App';
+    };
+  }, [count]);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}`}
+                      showLineNumbers
+                      codeBlock
+                      language="javascript"
+                      theme={dracula}
+                    />
+                  </div>
+
+                  <h2 class=" mt-4 text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
+                    3. useContext Hooks
+                  </h2>
+                  <p className="leading-relaxed text-justify">
+                    The <span className="text-[#ef443a]">useContext</span> Hook
+                    in React provides a way to subscribe to React context
+                    without adding nesting to your components.
+                  </p>
+                  <p className="text-pink-400 font-md "> Important Point : </p>
+                  <div className="text-justify">
+                    <li>
+                      <span className="text-rose-500">ThemeContext</span> is
+                      created with a default value of{" "}
+                      <span className="text-rose-500">Light</span>.
+                    </li>
+                    <li>
+                      <span className="text-green-500">ThemeProvider</span>{" "}
+                      component provides the{" "}
+                      <span className="text-green-500">Dark</span> theme value
+                      to its children.
+                    </li>
+                    <li>
+                      <span className="text-blue-500">ThemedButton</span>{" "}
+                      consumes the{" "}
+                      <span className="text-blue-500">ThemeContext</span> value
+                      using the{" "}
+                      <span className="text-blue-500">useContext</span> Hook and
+                      displays it.
+                    </li>
+                  </div>
+
+                  <div className="font-mono text-sm ">
+                    <CodeBlock
+                      text={`import React, { useContext, createContext } from 'react';
+// Step 1: Create the context
+const ThemeContext = createContext('Light');
+// Step 2: Provide the context value
+function ThemeProvider({ children }) {
+  const theme = 'Dark'; // This could be dynamic
+  return (
+    <ThemeContext.Provider value={theme}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+// Step 3: Consume the context value
+function ThemedButton() {
+  const theme = useContext(ThemeContext);
+  return <button>The theme is {theme}!</button>;
+}
+// Use the context in your app
+function App() {
+  return (
+    <ThemeProvider>
+      <ThemedButton />
+    </ThemeProvider>
+  );
+}
+export default App;`}
+                      showLineNumbers
+                      codeBlock
+                      language="javascript"
+                      theme={dracula}
+                    />
+                  </div>
+
+                  <h2 class=" mt-4 text-md  text-gray-900 title-font font-bold mb-2 font-amaranth ">
+                    4. useReducer Hooks
+                  </h2>
+                  <p className="leading-relaxed text-justify">
+                    An alternative to useState. Accepts a reducer of type
+                    <span className="text-[#ef443a]"> (state, action) = newState</span>, and returns the current state
+                    paired with a dispatch method. 
+                    <br/>
+                    Example: It is particularly
+                    useful when the state logic is complex and involves multiple
+                    sub-values, or when the next state depends on the previous
+                    one. useReducer is an alternative to useState. While
+                    useState is great for handling independent pieces of state,
+                    useReducer excels at handling more complex, interconnected
+                    state that involves multiple changes in an atomic and
+                    predictable manner.
+                  </p>
+                  <div className="font-mono text-sm ">
+                    <CodeBlock
+                      text={`import React, { useReducer } from 'react';
+// Initial State:
+const initialState = { count: 0 };
+// Reducer Function:
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+function Counter() {
+  // Using useReducer:
+  const [state, dispatch] = useReducer(reducer, initialState);
+    return (
+      <>
+        Count: {state.count}
+        // Updating State with Actions:
+        <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+        <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      </>
+    );
+  }`}
+                      showLineNumbers
+                      codeBlock
+                      language="javascript"
+                      theme={dracula}
+                    />
+                  </div>
+                  {/* <section class="text-gray-600 body-font">
                     <div class="container px-5 py-19 mt-3 mx-auto">
                       <div class="lg:w-3/3 w-full mx-auto overflow-auto">
                         <table class="table-auto w-full text-left whitespace-no-wrap">
@@ -755,7 +987,7 @@ return (
                         </table>
                       </div>
                     </div>
-                  </section>
+                  </section> */}
                 </DisclosurePanel>
               </Disclosure>
             </div>
@@ -811,7 +1043,7 @@ if (Math.random() < 0.5) {
                 >
                   <span
                     className="text-md font-semibold text-brand"
-                    style={{ color: textColor7}}
+                    style={{ color: textColor7 }}
                   >
                     Day6: Basic
                   </span>
@@ -921,7 +1153,7 @@ if (Math.random() < 0.5) {
             </div>
           </div>
 
-          <div class="w-full mt-4 flex bg-white dark:bg-dark_50 flex-col shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] false rounded-md border border-gray-200 white:border-zinc-800 hover:shadow-xl">
+          {/* <div class="w-full mt-4 flex bg-white dark:bg-dark_50 flex-col shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] false rounded-md border border-gray-200 white:border-zinc-800 hover:shadow-xl">
             <div class="px-3 py-2 border">
               <Disclosure as="div" defaultOpen={false}>
                 <DisclosureButton
@@ -999,7 +1231,7 @@ if (Math.random() < 0.5) {
                 </DisclosurePanel>
               </Disclosure>
             </div>
-          </div>
+          </div> */}
         </div>
       </main>
     </>
